88b3aef8b24504801ce71ced9c00f60454e5a985
tratchapong <tratchapong@gmail.com>
2025-02-09 15:28:24 +0700

    notFound + error Middlewares

---
+++ middlewares/error-middleware.js
@@ -0,0 +1,5 @@
+module.exports = (err, req, res, next) => {
+	console.log(err)
+	const statusCode = err.statusCode || 500
+	res.status(statusCode).json({error : err.message})
+}
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const notFound = require('./middlewares/not-found')
+const errorMiddleware = require('./middlewares/error-middleware')
 
 const app = express()
 
 app.use(express.json())
 
+
+app.use(notFound)
+app.use(errorMiddleware)
 
 const port = process.env.PORT || 9900
 app.listen(port, ()=> console.log('Server on port', port))
