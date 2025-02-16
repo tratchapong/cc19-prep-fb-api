3180bd304dbb99757564df61f7e9d91bf11ba6ad
tratchapong <tratchapong@gmail.com>
2025-02-16 22:49:01 +0700

    add tryCatch v2

--- utils/tryCatch.js
+++ utils/tryCatch.js
@@ -1,3 +1,13 @@
-module.exports = (func) => {
-	return (req, res, next) => func(req, res, next).catch(err => next(err))
+// module.exports = (func) => {
+// 	return (req, res, next) => func(req, res, next).catch(err => next(err))
+// }
+
+module.exports = (func)  => {
+	return async function (req, res, next) {
+		try {
+			await func(req, res, next)
+		}catch(err) {
+			next(err)
+		}
+	}
 }
